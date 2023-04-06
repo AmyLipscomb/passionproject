5e590233473217beff882b42c4b9f24b50721bd7
AmyLipscomb <amy.a.lipscomb@gmail.com>
2023-04-05 21:20:09 -0400

    started added some css styling

---
+++ src/components/Home/index.js
@@ -0,0 +1,23 @@
+const Home = () => {
+
+    return (
+
+        <body>
+            <div className="container">
+
+            </div>
+
+        </body>
+
+    )
+
+
+}
+
+export default Home;
+
+       // <div className="container">
+        //     <body>
+        //         <p className="mainPageTitle"> Amy's Essentials</p>
+        //     </body>
+        // </div>
