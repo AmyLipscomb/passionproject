22d0488bc661cef454f5da0ef91d9c56efb68b8f
AmyLipscomb <amy.a.lipscomb@gmail.com>
2023-04-23 10:09:27 -0400

    added content to page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,10 +3,27 @@ const Home = () => {
     return (
 
         <body>
-            <div className="container">
-
-            </div>
-
+        <header>
+            <h1>Amy's Essentials<span class="sen">sen</span>n</h1>
+    
+          {/* <!--Changed the 'div' tag to a navigation tag--> */}
+            <nav>
+                <ul>
+                    {/* <!--I learned how important the 'li'tag is. Including this tag, allowed the links on the right side of the header, to work properly. So, when you click on the links, you will be directed to the paragrah that correlates to the topic you selected.--> */}
+                    <li>
+                    <a href="#books">Books</a>
+                    &nbsp;
+                </li>
+                <li>
+                    <a href="#movies">Movies</a>
+                    &nbsp;
+                </li>
+                <li>
+                    <a href="#selfcare">Selfcare</a>
+                </li>
+                </ul>
+            </nav>
+        </header>
         </body>
 
     )
